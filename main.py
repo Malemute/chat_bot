6a9043a227facc792b22b51cb49f1cffe1223b0a
Malemute <eugeny.mamontov@gmail.com>
2020-03-12 22:03:19 +0300

    the first real module. all the previous was drafts

--- main.py
+++ main.py
@@ -4,39 +4,38 @@ import argparse
 
 def check_tasks(token):
 
-  authoriz_template = "Token {}"
-  request_headers = {"Authorization":
-    authoriz_template.format(token)}
-  #request_params = {
-  #  "timeout": 60
-  }
-  request_url = 'https://dvmn.org/api/long_polling/'
+    authoriz_template = "Token {}"
+    request_headers = {"Authorization":
+      authoriz_template.format(token)}
+    request_url = 'https://dvmn.org/api/long_polling/'
   #request_url = 'https://dvmn.org/api/user_reviews/'
 
-  response = requests.get(request_url, 
-    headers = request_headers,
-#    params = request_params
-     timeout = 60
-   )
-  response.raise_for_status()
-  tasks_structure = response.json()
-  
-  while True:
-#    request_params = {
-#      "timeout": 60
-    }
-    response = requests.get(request_url, 
-      headers = request_headers,
-      timeout = 60
-      )
-    for message in response:
-      bot.answer(message)
-	  
-  return tasks_structure
+    request_params = ""
+
+    while True:
+
+        response = requests.get(request_url,
+            headers = request_headers,
+            params = request_params
+            timeout = 60
+        )
+        response.raise_for_status()
+        tasks_structure = response.json()
+        if tasks_structure.status == "found":
+            timestamp = tasks_structure.last_attempt_timestamp
+        else:
+            timestamp = tasks_structure.timestamp_to_request
+
+        request_params = {
+            "timestamp": timestamp
+        }
+
+    return tasks_structure
 
 
 if __name__ == '__main__':
-  token = os.getenv("DEVMAN_TOKEN")
-  print_info = check_tasks(token)
-  print(print_info)
+    token = os.getenv("DEVMAN_TOKEN")
+    print_info = check_tasks(token)
+    print(print_info)
+
 
